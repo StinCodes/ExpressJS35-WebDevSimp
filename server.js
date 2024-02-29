5e4c1a50e608267c9d093f6be37949acd1d9e33b
Austin Song <austinhssong@gmail.com>
2024-02-29 12:27:20 -0500

    rendering html completed

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
+
+//set up app view engine with ejs
+app.set("view engine", "ejs");
 
 //navigating to / path prints "Here" to console and json "Error" to page
-app.get('/', (req, res)=>{
-  console.log('Here')
-  res.json({message : "Error"})
-})
+// app.get('/', (req, res)=>{
+//   console.log('Here')
+//   res.json({message : "Error"})
+// })
+
+//render the index.html file with ejs
+app.get("/", (req, res) => {
+  res.render("index", { text: "World" });
+});
 
 //set app to listen on port 3000
-app.listen(3000)
+app.listen(3000);
