7ccea55198764e22c2aecc89f7a5ef464a8fa84d
Austin Song <austinhssong@gmail.com>
2024-02-29 12:01:26 -0500

    sending data completed

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
 
-app.get()
+//navigating to / path prints "Here" to console and json "Error" to page
+app.get('/', (req, res)=>{
+  console.log('Here')
+  res.json({message : "Error"})
+})
 
+//set app to listen on port 3000
 app.listen(3000)
